b9ada45669c6b98cf77036f9ce1928f3f10c63c2
bharathabhi46 <bharathabhibalne46@gmail.com>
2025-12-14 16:08:40 +0530

    Fix Netlify routing and config

--- middleware/loginSignin.js
+++
@@ -1,8 +0,0 @@
-export function logSignin(req, res, next) {
-  if (!req.session.userId) {
-    console.log("Access to protected route blocked");
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  next();
-}
